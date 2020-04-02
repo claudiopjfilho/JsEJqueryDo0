b607562ff086624bcaadeeaac473e6fd2d97e91b
Claudio Filho <54292133+claudiopjfilho@users.noreply.github.com>
2020-04-02 10:53:09 -0300

    JS

---
+++ JS/script2.js
@@ -0,0 +1,10 @@
+var numero = 6;
+var decimal = 4.5;
+var string = "Olá Mundo";
+var lista = ["Maçã", "Laranja", "Banana", 1234];
+
+for(i in lista){
+    alert(lista[i]);
+}
+
+console.log("Olá Mundo!");
